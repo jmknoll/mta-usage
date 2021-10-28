4c873bc8e84b95867ba414aac03d52dc577871b3
Jameson Knoll <j.knoll621@gmail.com>
2021-10-28 12:53:08 -0400

    new deploy

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,7 +1,6 @@
 #!/bin/sh
 
 set -e
-flask db init
 flask db migrate
 flask db upgrade
 gunicorn --worker-class gevent --workers 8 --bind 0.0.0.0:5000 wsgi:app --max-requests 10000 --timeout 5 --keep-alive 5 --log-level info
